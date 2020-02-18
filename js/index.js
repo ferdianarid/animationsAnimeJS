d96fbb5b1af7b2fb7377582ea90554dedc133d26
ferdian <ferdianahmadrozikin018@gmail.com>
2020-02-18 08:03:45 +0700

    animation

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const companies = [
+    {name : "company one", category: "finance", start: 1981, end: 2003},
+    {name : "company two", category: "retail", start: 1992, end: 2007},
+    {name : "company three", category: "auto", start: 1999, end: 2008},
+    {name : "company four", category: "retail", start: 1989, end: 2012},
+    {name : "company five", category: "technology", start: 2009, end: 2000},
+    {name : "company six", category: "finance", start: 1987, end: 2009},
+    {name : "company seven", category: "auto", start: 1986, end: 2010},
+    {name : "company eight", category: "technology", start: 2011, end: 2015},
+    {name : "company nine", category: "retail", start: 1981, end: 2019}
+];
+
+// for (let i = 0; i < companies.length; i++ ) {
+//     console.log(companies[i].category);
+// }
+
+
